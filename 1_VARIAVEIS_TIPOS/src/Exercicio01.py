c7029647661993d59803bb8c254f144c9d703ac3
viniciusfga <viniciusfg.araujo@gmail.com>
2025-11-07 23:25:58 -0300

    Primeiro exercicio

---
+++ 1_VARIAVEIS_TIPOS/src/Exercicio01.py
@@ -0,0 +1,9 @@
+inteiro = 3
+decimal = 3.14
+palavra = 'Python'
+condicao = True
+
+print(inteiro)
+print(decimal)
+print(palavra)
+print(condicao)
---
+++ 1_VARIAVEIS_TIPOS/src/Exercicio02.py
@@ -0,0 +1,5 @@
+inteiro = 1
+decimal = 3.14
+
+print(type(inteiro))
+print(type(decimal))
