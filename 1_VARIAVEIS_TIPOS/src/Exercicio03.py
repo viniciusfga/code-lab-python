e759235d3e6f78f0321dbf377eba83949bb140fc
viniciusfga <viniciusfg.araujo@gmail.com>
2025-11-08 17:33:29 -0300

    Alguns exercícios adicionados

---
+++ 1_VARIAVEIS_TIPOS/src/Exercicio03.py
@@ -0,0 +1,20 @@
+# Declaração de variáveis
+a = 8
+b = 4
+c = 2
+
+# ADIÇÃO
+soma = a + b + c
+print("SOMA:", a, "+", b, "+", c, "=", soma)
+
+# SUBTRAÇÃO
+subtracao = a - b - c
+print("SUBTRAÇÃO:", a, "-", b, "-", c, "=", subtracao)
+
+# MULTIPLICAÇÃO
+multiplicacao = a * b * c
+print("MULTIPLICAÇÃO:", a, "*", b, "*", c, "=", multiplicacao)
+
+# DIVISÃO
+divisao = a / b / c
+print("DIVISÃO:", a, "/", b, "/", c, "=", divisao)
---
+++ 3_CONDICIONAIS/Exercicio16.py
@@ -0,0 +1,4 @@
+a = int(input("Digite um valor: "))
+
+if a > 0: print("Número positivo")
+else: print("Número negativo")
