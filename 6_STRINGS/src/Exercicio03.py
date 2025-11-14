4a090b5dfc93c8c832a2df588ac8dc67acb2ac58
viniciusfga <viniciusfg.araujo@gmail.com>
2025-11-13 22:16:12 -0300

    Exercicio04 Strings

--- 6_STRINGS/src/Exercicio03.py
+++ 6_STRINGS/src/Exercicio03.py
@@ -1,14 +1,11 @@
 #Verificar se uma palavra é palíndromo (ex: “arara”).
 
 palavra = "arara"
+def is_palindromo(palavra):
+    return palavra == palavra[::-1]
 
-def is_palindrome(palavra : str) -> bool:
-    cleaned = ''.join(ch for ch in palavra.lower() if ch.isalnum())
-    return cleaned == palavra[::-1]
-
-if __name__ == '__main__':
     print(is_palindrome("arara"))
-if is_palindrome(palavra):
+if is_palindromo(palavra):
     print(f"{palavra} é palíndromo")
 else:
     print(f"{palavra} não é palíndromo")
---
+++ 6_STRINGS/src/Exercicio04.py
@@ -0,0 +1,4 @@
+frase = "Eu gosto de estudar java"
+print(frase)
+nova_frase = frase.replace("java", "Python")
+print(nova_frase)
